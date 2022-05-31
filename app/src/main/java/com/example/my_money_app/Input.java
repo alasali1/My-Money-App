9f884a5dcc7aa8cbf0a83715dbac0bf90001daed
DipendraShiwakoti <dipendra.shiwakoti@go.shoreline.edu>
2022-05-31 13:22:11 -0700

    added input.java file

--- app/src/main/java/com/example/my_money_app/Input.java
+++ app/src/main/java/com/example/my_money_app/Input.java
@@ -3,16 +3,12 @@ package com.example.my_money_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Input extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void onDobClick(View view) {
+        setContentView(R.layout.activity_input);
     }
 }
